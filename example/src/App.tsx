7dac187324325b5a23cb567877557ce00212755d
Tushar Sanchaniya <82019401+TusharSanchaniya-mi@users.noreply.github.com>
2023-04-25 11:10:48 +0530

    Git Issue #9 (#10)
    * fixed issue of https://github.com/Mindinventory/react-native-bus-seat-layout/issues/9
    
    * fixed issue of https://github.com/Mindinventory/react-native-bus-seat-layout/issues/9
    
    * + Uncommented lines that shows number when user clicks on any seat to book.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <SafeAreaView>
       <SeatsLayout
-        row={13}
+        row={4}
         layout={{ columnOne: 2, columnTwo: 2 }}
         isSleeperLayout={true}
         seatImage={{ image: SleeperSeatIcon, tintColor: 'gray' }}
@@ -16,11 +16,6 @@ export default function App() {
           { seatNumber: 11, seatType: 'women' },
           { seatNumber: 16, seatType: 'booked' },
           { seatNumber: 17, seatType: 'women' },
-          { seatNumber: 22, seatType: 'women' },
-          { seatNumber: 39, seatType: 'women' },
-          { seatNumber: 41, seatType: 'booked' },
-          { seatNumber: 42, seatType: 'women' },
-          { seatNumber: 43, seatType: 'women' },
         ]}
         numberTextStyle={styles.numberStyle}
         getBookedSeats={(seats) => {
